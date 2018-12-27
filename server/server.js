cfb9c0d09ad2998a69315e9591a39c4afd724c9b
hadesunseenn <hadesunseenn@gmail.com>
2018-12-27 11:50:37 -0500

    socket.io added

--- server/server.js
+++ server/server.js
@@ -1,23 +1,28 @@
 const path = require('path');
+const http = require('http');
+
 const express  = require('express');
+const socketIO  = require('socket.io');
 var hbs = require('hbs');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 //we can access all the files in this dir from browser like /help.html
 app.use(express.static(publicPath));
 app.set('view engine', 'hbs');
 
-// app.get('/home', (req, res)=>{
-// 	// res.send('This is about page.');
-// 	res.render('about.hbs',{
-// 		pageTitle:'About page'
-// 	});
-// });
+io.on('connection', (socket)=>{
+	console.log('New user connected');
 
-// app.use(bodyParser.json());
-app.listen(port, ()=>{
+	socket.on('disconnect', ()=>{
+		console.log('disconnected from server');
+	});
+});
+
+server.listen(port, ()=>{
 	console.log(`started on port ${port}`);
 });
